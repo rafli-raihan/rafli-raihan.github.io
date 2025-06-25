bae6b60645a670ef2c7f456fd1435b06263eba9c
rafli-raihan <mrafliraihan@proton.me>
2025-06-26 01:05:31 +0700

    klo di home gaada footer

--- src/App.vue
+++ src/App.vue
@@ -15,13 +15,13 @@ const isRoot = useIsOnRoot()
     <RouterView />
   </main>
 
-  <footer>
+  <footer v-if="!isRoot">
     <nav>
       <a href="https://www.facebook.com/fli.raihan/">
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
           <path
             fill="currentColor"
-            d="M23 9v6h-1v2h-1v2h-1v1h-1v1h-2v1h-2v1h-1v-8h2v-1h1v-2h-3V9h1V8h2V5h-4v1h-2v2h-1v4H7v3h3v8H9v-1H7v-1H5v-1H4v-1H3v-2H2v-2H1V9h1V7h1V5h1V4h1V3h2V2h2V1h6v1h2v1h2v1h1v1h1v2h1v2z"
+            d="M23 9v6h-1v2h-1v2h-1v1h-1v1h-1v1h-2v1h-1v-8h2v-1h1v-2h-3V9h1V8h2V5h-4v1h-2v2h-1v4H7v3h3v8H9v-1H7v-1H5v-1H4v-1H3v-2H2v-2H1V9h1V7h1V5h1V4h1V3h2V2h2V1h6v1h2v1h2v1h1v1h1v2h1v2z"
           />
         </svg>
       </a>
@@ -76,7 +76,6 @@ main {
 
 footer {
   position: relative;
-  margin-top: 100vh;
   width: 100%;
   display: block;
   align-items: center;
